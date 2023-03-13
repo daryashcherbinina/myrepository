58bd9fff0ed2c1e027ef7ac066567df66b86ce0e
daryashcherbinina <ludmila670502@gmail.com>
2023-03-13 21:50:34 +0700

    aa

--- pr1.js
+++ pr1.js
@@ -1,20 +1,24 @@
-var s = "123'56'df";
-var cisl = "";
-var num = "";
+function insert(str, substr, pos) {
+    var array = str.split('');
+    array.splice(pos, 0, substr);
+    return array.join('');
+}
+var s = "'8'23'5'df'4'ty'3'";
+var code = "";
 for (let i = 0; i < s.length; i++) {
     if (s[i] == "'") {
-        for (let j = i+1; j < s.length; j++){
+        for (let j = i + 1; j < s.length; j++) {
             if (s[j] == "'") {
-                i = j;
+                s = insert(s, code, i);
+                s = s.slice(0, i + code.length) + s.slice(j + 1 + code.length);
+                i = j + code.length - 1;
+                code = "";
                 break;
             }
-            cisl = String(s.charCodeAt(j));
-            s[i] = cisl;
-            alert(s[i]);
-            
+            code += String(s.charCodeAt(j));
         }
-    
     }
-    
 }
+alert(s);
+
 
