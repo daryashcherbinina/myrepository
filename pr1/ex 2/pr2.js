249a9d32040b3b253c0068f7f0ba1edb41d50d94
daryashcherbinina <ludmila670502@gmail.com>
2023-03-21 00:06:25 +0700

    fgfg

--- pr1/ex 2/pr2.js
+++ pr1/ex 2/pr2.js
@@ -1,13 +1,27 @@
-class Person {
-  constructor(letter, method) {
-    this.letter = letter;
-    this.metod = method;
-  }
-    check() {
-        console.log(this.letter, this.metod);
+
+function score(letter,first,second) {
+    const props1 = Object.values(first);
+    const props2 = Object.values(second);
+    alert(letter == props1[0].charAt(0));
+    let scoreF = 0;
+    let scoreS = 0;
+    for (let i = 0; i < props1.length; i += 1) {
+            if (props1[i] == "" && props2[i] != "" && props1[i].charAt(0) == letter) {
+                scoreS += 1;
+            }
+            if (props2[i] == "" && props1[i] != "" && props2[i].charAt(0) == letter) {
+                scoreF += 1;
+            }
+            if (props1[i] != props2[i] && (props1[i] != "" || props2[i] != "")) {
+                scoreF += 1;
+                scoreS += 1;
+            }
+        }
+        formTable(first,second,{scoreF}, {scoreS}, ".wrapper");
     }
-}
-function check(object1, object2)
+    
+
+
 function Random() {
     let s = 'АБВГДЕЖЗИКЛМНОПРСТУФХЦЧШЩЭЮЯ'
     return s[Math.floor(Math.random()*28)];
@@ -18,35 +32,89 @@ btn.onclick = function () {
     div.innerHTML = Random();
 }
 
-const {form} = document.forms;
-function retrieveFormValue(event) {
-    event.preventDefault();
-    const {name, city, plant, animal, river} = form; //деструкторизация
-    const values = {
+function object() {
+    
+     //деструкторизация
+    
+    /*let values = {
         name: name.value,
         city: city.value,
         plant: plant.value,
         animal: animal.value,
         river: river.value
     };
+    return values;*/
+}
+let values1;
+let values2;
+function retrieveFormValue(event) {
+    event.preventDefault();
+    let {form} = document.forms;
+    let {name, city, plant, animal, river} = form;
+    
     let letter = document.getElementById('letter').innerHTML;
     let player = document.getElementById('player').innerHTML;
+    
     if (player == 1) {
-        const One = new Person(letter,values);
-         One.saySo();
+        values1 = {
+        name: name.value,
+        city: city.value,
+        plant: plant.value,
+        animal: animal.value,
+        river: river.value
+    }
         player = 2;
         document.getElementById('player').innerHTML = player;
     }
     else {
-        const Two = new Person(letter,values);
+        values2 = {
+        name: name.value,
+        city: city.value,
+        plant: plant.value,
+        animal: animal.value,
+        river: river.value
+        }
         player = 1;
         document.getElementById('player').innerHTML = player;
         div.innerHTML = Random();
+        score(letter,values1,values2);
+        
     }
+    
     var inputs = document.querySelectorAll('input[type=text]');
     for (var i = 0;  i < inputs.length; i++) {
     inputs[i].value = '';
-    };
+    }; 
+    
 }
 form.addEventListener('submit', retrieveFormValue);
+
+function formTable(first, second, scoreF, scoreS, selector) {
+    var wrapper = document.querySelector(selector);
+    var player1 = { player: '1' }
+    var player2 = { player:'2' }
+    const str1 = Object.assign(player1,first, scoreF);
+    const str2 = Object.assign(player2,second, scoreS);
+    const props1 = Object.values(str1);
+    const props2 = Object.values(str2);
+    console.log(first);
+    var table = wrapper.getElementsByTagName("table")[0];
+    var tr = document.createElement("tr"), td;
+    for(let i = 0; i < props1.length; i += 1) {
+      td = document.createElement("td");
+      td.innerHTML = props1[i];
+      tr.appendChild(td);
+    }    
+     table.appendChild(tr); 
+    var table = wrapper.getElementsByTagName("table")[0];
+    var tr = document.createElement("tr"), td;
+    for(let j = 0; j < props2.length; j += 1) {
+      td = document.createElement("td");
+      td.innerHTML = props2[j];
+      tr.appendChild(td);
+    }    
+    table.appendChild(tr); 
+    
+}
+
 
